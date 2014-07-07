56b9df539b44205ec9379f3981712984f47b8a9c
Steffan Voges <steffan@berkeley.edu>
2014-07-06 20:08:53 -0700

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Steffan Voges on 7/6/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+    }
+}
